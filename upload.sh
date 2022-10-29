364b17fa56e0c08195af4ac76680df02dd9b39f3
treboris <blubosz@gmail.com>
2022-10-29 20:37:54 +0200

    bash_test

--- upload.sh
+++ upload.sh
@@ -2,9 +2,15 @@
 
 clear
 
+
+token = "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"
+user = "treboris"
+
 printf "Commit messege: "
 read asd
 
 git add .
 git commit -m "$asd"
-git push -u origin master | "treboris"
+git push -u origin master
+$user
+$token
