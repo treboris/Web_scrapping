f45c6986133deebb149a9a70ede9d940b84d2ad2
treboris <blubosz@gmail.com>
2023-01-21 14:21:17 +0100

    treboris

--- sidefunctions/upload.sh
+++ sidefunctions/upload.sh
@@ -10,5 +10,5 @@ read message
 git add .
 git commit -m "$message"
 git push -u origin master | treboris
-treboris
-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
+send treboris
+send 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
