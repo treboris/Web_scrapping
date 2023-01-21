ac42cb21bb6e9633dd7a268b6efd4fa6503528cf
treboris <blubosz@gmail.com>
2023-01-21 14:23:00 +0100

    upload

--- sidefunctions/upload.sh
+++ sidefunctions/upload.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -f
 
 clear
 
@@ -10,5 +10,10 @@ read message
 git add .
 git commit -m "$message"
 git push -u origin master | treboris
-send treboris
-send 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
+
+expect "username: " # write the keyword you get as prompt for entering username
+send "treboris\r"
+# remove the below 2 lines if you do not want script to interact for password.
+expect "password: " # write the keyword you get as prompt for entering password
+send "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\r"
+interact
